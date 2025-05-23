79f53d04eef3f47ec5ec1e33d8d676a1ba437f3f
Irineu <matadorDePorco@gmail.com>
2025-05-22 22:59:36 -0300

    messi

--- biblioteca.py
+++ biblioteca.py
@@ -1,10 +1,10 @@
 import json
 class Livro:
-    def __init__(self, titulo, autor, ano_publicacao, id, status="Disponível"):
+    def __init__(self, titulo, autor, ano_publicacao, ID, status="Disponível"):
         self.titulo = titulo
         self.autor = autor
         self.ano_publicacao = ano_publicacao
-        self.id = id
+        self.ID = ID
         self.status = status
 
     def editar_livro(self, novo_titulo, novo_autor, novo_ano_publicacao):
@@ -17,19 +17,37 @@ class Livro:
         pass
 
 class Biblioteca:
-    def __init__(self):
-        self.livros = []
+    def __init__(self, nome="Alex Library"):
+        self.nome = nome
 
     def adicionar_livro(self, livro):
-        pass
+        #save_to_json
+        livroDict = {
+            "titulo": livro.titulo,
+            "autor": livro.autor,
+            "ano_publicacao": livro.ano_publicacao,
+            "id": livro.ID,
+            "status": livro.status
+        }
+        with open("livros.json", "w") as f:
+            f.write(json.dumps(livroDict, indent=1))
 
     def remover_livro(self, livro):
-        #save_to_json
         pass
 
     def listar_livros(self):
         #load_from_json
-        pass
+        with open("livros.json", "r") as f:
+            data = json.loads(f.read())
+            if data == "":
+                print("Nenhum livro cadastrado")
+            else:
+                for x in range(1):
+                    print(f"ID: {data['id']}")
+                    print(f"Título: {data['titulo']}")
+                    print(f"Autor: {data['autor']}")
+                    print(f"Ano de Publicação: {data['ano_publicacao']}")
+                    print(f"Status: {data['status']}",end="\n\n")
 
     def buscar_livro(self, titulo):
         pass
--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from biblioteca import Biblioteca, Livro
+import random
+nomeBiblioteca = input("Digite o nome da biblioteca: ")
+biblioteca = Biblioteca(nomeBiblioteca)
 
 def main():
-    print("Hello from trabalho-biblioteca-uv!")
+    print("\nGerenciador de Biblioteca\n" )
+    while True:
+        print(f"\n{biblioteca.nome}\n")
+        print("Escolha oque deseja fazer")
+        print("1. Adicionar Livro")
+        print("2. Listar Livros")
+        print("3. Editar Livro")
+        print("4. Excluir Livro")
+        print("5. Buscar Livro")
+        print("6. Sair")
+        opcao = input("Escolha uma opção: ")
+        print("")
+        if opcao == "1":
+
+            livroTitulo = input("Digite o título do livro: ")  
+            livroAutor = input("Digite o autor do livro: ")
+            livroAnoDePublicacao = input("Digite o ano de publicação do livro: ")
+
+            if livroAnoDePublicacao.isdigit() == False:
+
+                print("Por favor digite um numero valido")
+
+            
+            randomID = str(random.randint(1000, 9999))
+            NovoLivro = Livro(livroTitulo, livroAutor, livroAnoDePublicacao, randomID,"Disponivel")
+            biblioteca.adicionar_livro(NovoLivro)
+
+        elif opcao == "2":
+            biblioteca.listar_livros()
+        elif opcao == "6":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
 
 
 if __name__ == "__main__":
