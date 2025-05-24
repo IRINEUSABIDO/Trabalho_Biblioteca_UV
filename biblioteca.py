d0ad785325218ae6a8a22119c41d89b1e36671e8
Irineu <matadorDePorco@gmail.com>
2025-05-24 19:33:53 -0300

    editar livro ta dando pau

--- biblioteca.py
+++ biblioteca.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Livro:
     def __init__(self, titulo, autor, ano_publicacao, ID, status="Disponível"):
         self.titulo = titulo
@@ -8,55 +9,94 @@ class Livro:
         self.ID = ID
         self.status = status
 
-    def editar_livro(self, novo_titulo, novo_autor, novo_ano_publicacao):
-        pass
-
     def marcar_como_emprestado(self):
         pass
-    
+
     def marcar_como_disponivel(self):
         pass
+
 
 class Biblioteca:
     def __init__(self, nome="Alex Library"):
         self.nome = nome
 
     def adicionar_livro(self, livro):
-        #save_to_json
+        # save_to_json
         livroDict = {
             "titulo": livro.titulo,
             "autor": livro.autor,
             "ano_publicacao": livro.ano_publicacao,
             "id": livro.ID,
-            "status": livro.status
+            "status": livro.status,
         }
         with open("livros.json", "r") as f:
             data = json.loads(f.read())
 
-
         data["lista_livros"].append(livroDict)
-
 
         with open("livros.json", "w") as f:
             f.write(json.dumps(data, indent=1))
 
-    def remover_livro(self, livro):
-        pass
+    def remover_livro(self, livroID):
+        with open("livros.json", "r") as f:
+            data = json.loads(f.read())
+            nova_data = [
+                livro for livro in data["lista_livros"] if livro["id"] != livroID
+            ]
+
+        if nova_data == data["lista_livros"]:
+            return False
+
+        else:
+            data["lista_livros"] = nova_data
+            with open("livros.json", "w") as f:
+                f.write(json.dumps(data, indent=1))
+            return True
+
+    def editar_livro(self, novo_titulo, novo_autor, novo_ano_publicacao, livroID):
+        with open("livros.json", "r") as f:
+            data = json.loads(f.read())
+            data2 = json.loads(f.read())
+
+        for livro in data["lista_livros"]:
+            if livro["id"] == livroID:
+                livro["titulo"] = novo_titulo
+                livro["autor"] = novo_autor
+                livro["ano_publicacao"] = novo_ano_publicacao
+
+        if data == data2:
+            return False
+
+        else:
+            with open("livros.json", "w") as f:
+                f.write(json.dumps(data, indent=1))
+
+            return True
 
     def listar_livros(self):
-        #load_from_json
+        # load_from_json
         with open("livros.json", "r") as f:
             data = json.loads(f.read())
             if data == "":
                 print("Nenhum livro cadastrado")
             else:
-                for x in range(1):
-                    print(f"ID: {data['id']}")
-                    print(f"Título: {data['titulo']}")
-                    print(f"Autor: {data['autor']}")
-                    print(f"Ano de Publicação: {data['ano_publicacao']}")
-                    print(f"Status: {data['status']}",end="\n\n")
+                for livro in data["lista_livros"]:
+                    print(f"ID: {livro['id']}")
+                    print(f"Título: {livro['titulo']}")
+                    print(f"Autor: {livro['autor']}")
+                    print(f"Ano de Publicação: {livro['ano_publicacao']}")
+                    print(f"Status: {livro['status']}", end="\n\n")
 
     def buscar_livro(self, titulo):
-        pass
-
+        livro_encontrado = False
+        with open("livros.json", "r") as f:
+            data = json.loads(f.read())
+            for livro in data["lista_livros"]:
+                if livro["titulo"] == titulo:
+                    print(f"ID: {livro['id']}")
+                    print(f"Título: {livro['titulo']}")
+                    print(f"Autor: {livro['autor']}")
+                    print(f"Ano de Publicação: {livro['ano_publicacao']}")
+                    print(f"Status: {livro['status']}", end="\n\n")
+                    livro_encontrado = True
+        return livro_encontrado
--- main.py
+++ main.py
@@ -2,17 +2,12 @@ from biblioteca import Biblioteca, Livro
 import random
 import json
 
+
 biblioteca = Biblioteca("Alex Library")
-
-with open ("livros.json", "w") as f:
-    f.write(json.dumps({ "lista_livros" : [
-
-    ]}, indent=1))
-
 
 
 def main():
-    print("\nGerenciador de Biblioteca\n" )
+    print("\nGerenciador de Biblioteca\n")
     while True:
         print(f"\n{biblioteca.nome}\n")
         print("Escolha oque deseja fazer")
@@ -25,25 +20,93 @@ def main():
         opcao = input("Escolha uma opção: ")
         print("")
         if opcao == "1":
-
-            livroTitulo = input("Digite o título do livro: ")  
+            livroTitulo = input("Digite o título do livro: ")
             livroAutor = input("Digite o autor do livro: ")
             livroAnoDePublicacao = input("Digite o ano de publicação do livro: ")
 
             if livroAnoDePublicacao.isdigit() == False:
-
                 print("Por favor digite um numero valido")
 
-            
-            randomID = str(random.randint(1000, 9999))
-            NovoLivro = Livro(livroTitulo, livroAutor, livroAnoDePublicacao, randomID,"Disponivel")
+            with open("livros.json", "r") as f:
+                data = json.loads(f.read())
+                randomID = str(random.randint(1000, 9999))
+                for livro in data["lista_livros"]:
+                    if livro["id"] == randomID:
+                        randomID = str(random.randint(1000, 9999))
+
+            NovoLivro = Livro(
+                livroTitulo, livroAutor, livroAnoDePublicacao, randomID, "Disponivel"
+            )
             biblioteca.adicionar_livro(NovoLivro)
 
         elif opcao == "2":
             biblioteca.listar_livros()
+
+        elif opcao == "3":
+            livroID = input("Digite o ID do livro que deseja editar: ")
+            livroNovoTitulo = input("Digite o novo título do livro: ")
+            livroNovoAutor = input("Digite o novo autor do livro: ")
+            livroNovoAnoDePublicacao = input(
+                "Digite o novo ano de publicação do livro: "
+            )
+
+            if livroNovoAnoDePublicacao.isdigit() == False:
+                print("Por favor digite um numero de publicaçao valido")
+
+            elif livroID.isdigit() == False:
+                print("Por favor digite um numero de ID valido")
+
+            else:
+
+                try:
+                    resultado = biblioteca.editar_livro(
+                        livroNovoTitulo,
+                        livroNovoAutor,
+                        livroNovoAnoDePublicacao,
+                        livroID,
+                    )
+                    if resultado == True:
+                        print("Livro editado com sucesso")
+                    else:
+                        print("Livro não encontrado")
+
+                except:
+                    print("Ocorreu um erro ao tentar editar o livro")
+
+        elif opcao == "4":
+            livroID = input("Digite o ID do livro que deseja excluir: ")
+            if livroID.isdigit() == False:
+                print("Por favor digite um numero de ID valido")
+
+            else:
+                try:
+                    resultado = biblioteca.remover_livro(livroID)
+                    if resultado == True:
+                        print("Livro excluido com sucesso")
+                    else:
+                        print("Livro não encontrado")
+                except:
+                    print("Ocorreu um erro ao tentar excluir o livro")
+
+        elif opcao == "5":
+            livroTitulo = input("Digite o título do livro que deseja buscar: ")
+            print("")
+
+            try:
+                print("Livros encontrados:")
+                resultado = biblioteca.buscar_livro(livroTitulo)
+                if resultado == False:
+                    print("Nenhum livro encontrado")
+
+            except:
+                print(
+                    "Ocorreu um erro ao tentar buscar o livro, verefique o titulo ou se o livro existe"
+                )
+
         elif opcao == "6":
             print("Saindo...")
             break
+
         else:
             print("Opção inválida. Tente novamente.")
 
